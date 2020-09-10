1f7c43e71e7d63bda32bbafceb8906ce158d4beb
Aron <arontheking@gmail.com>
2020-09-10 13:59:10 +0000

    add homework to lesson 1

---
+++ 1/MatrixMultiplication.java
@@ -0,0 +1,18 @@
+// Szorgalmi feladat
+
+// Az $A \in \mathbb{R}^{m \times n}$ és a $B \in \mathbb{R}^{n \times p}$ mátrixok
+// szorzata egy $C \in \mathbb{R}^{m \times p}$ mátrix, melynek elemei a következők:
+
+ 
+
+// $$
+// c_{i,j} = \sum\limits_{k=1}^{n} a_{ik} b_{k_j}, \quad i = 1,\ldots,m, \quad j = 1,\ldots, p
+// $$
+
+ 
+
+// 1. Implementáld a mátrixszorzást Java tömbökön! 
+// 2. Mérd meg a futási időt!
+// 3. Vizsgáld meg, milyen hatással van a belső for-ciklusok felcserélése a futási időre!
+
+public class MatrixMultiplication {}
